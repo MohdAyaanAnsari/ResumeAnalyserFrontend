ee8e1829a2e7a0ac1b7044ff30f44b0a4d675e3c
Ayaan <ayaanansari9997843139@gmail.com>
2026-04-22 20:18:39 +0530

    Update base.ts

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://resumeanalyserbackend-lnr4.onrender.com/api', // Your backend base URL
+  baseURL: 'https://resumeanalyserbackend-ixbr.onrender.com/api', // Your backend base URL
   timeout: 30000, // AI analysis can take a while
 })
 
